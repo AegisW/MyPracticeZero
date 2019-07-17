34c746cd7bb4280125879600ba45e30d61e9eb98
AegisW <ewei337@hotmail.com>
2019-07-17 17:45:20 +1000

    tutorial 5

--- Tutorial/tut5/MyFirstWebsite/MyFirstWebsite/Startup.cs
+++ Tutorial/tut5/MyFirstWebsite/MyFirstWebsite/Startup.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MyFirstWebsite.Data;
 
 namespace MyFirstWebsite
 {
@@ -20,7 +23,15 @@ namespace MyFirstWebsite
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddDbContext<SchoolContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>

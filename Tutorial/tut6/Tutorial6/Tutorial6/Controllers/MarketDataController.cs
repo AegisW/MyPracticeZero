2eb5f0358a7d0632ee28ffa2562c4a525c21c632
AegisW <ewei337@hotmail.com>
2019-07-24 21:06:39 +1000

    Tutorial 6 update

---
+++ Tutorial/tut6/Tutorial6/Tutorial6/Controllers/MarketDataController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tutorial6.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tutorial6.Controllers
+{
+    [Route("api/[controller]")]
+    public class MarketDataController : Controller
+    {
+        private readonly ASXAuditTrailContext db;
+        public MarketDataController(ASXAuditTrailContext sc)
+        {
+            db = sc;
+        }
+
+        // GET api/<controller>/
+        [HttpGet("{code}/{offset}/{rows}")]
+        public List<MarketChartPrice> Get(string code, int offset, int rows)
+        {
+            return db.GetMarketChartPricesAsync(code, offset, rows);
+        }
+    }
+}

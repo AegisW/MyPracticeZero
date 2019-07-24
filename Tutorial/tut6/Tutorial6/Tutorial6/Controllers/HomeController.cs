005ade832af0b2a1e29c49a4d03acd32b3d78b81
AegisW <ewei337@hotmail.com>
2019-07-24 20:09:28 +1000

    Tutorial 6

--- Tutorial/tut6/Tutorial6/Tutorial6/Controllers/HomeController.cs
+++ Tutorial/tut6/Tutorial6/Tutorial6/Controllers/HomeController.cs
@@ -5,14 +5,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Tutorial6.Models;
+using Tutorial6.ViewModels;
 
 namespace Tutorial6.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ASXAuditTrailContext db;
+        public HomeController(ASXAuditTrailContext sc)
+        {
+            db = sc;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            Home home = new Home()
+            {
+                SecCode = "SAM",
+                OffsetCount = 0,
+                RowCount = 300,
+            };
+            home.MarketChartPrice = db.GetMarketChartPricesAsync(home.SecCode, home.OffsetCount, home.RowCount);
+            return View(home);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Home home)
+        {
+            home.MarketChartPrice = db.GetMarketChartPricesAsync(home.SecCode, home.OffsetCount, home.RowCount);
+            return View(home);
         }
 
         public IActionResult Privacy()
---
+++ Tutorial/tut6/Tutorial6/Tutorial6/ViewModels/Home.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tutorial6.Models;
+
+namespace Tutorial6.ViewModels
+{
+    public class Home
+    {
+        public string SecCode { get; set; }
+        public int OffsetCount { get; set; }
+        public int RowCount { get; set; }
+        public List<MarketChartPrice> MarketChartPrice { get; set; }
+    }
+}
